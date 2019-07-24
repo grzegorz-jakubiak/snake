e5eee8b87cbfc542daf75f2a55c550249c1136d3
Grzegorz Jakubiak <grzegorz.jakubiak@outlook.com>
2019-07-24 17:04:54 +0200

    Fix directions

--- src/consts/directions.js
+++ src/consts/directions.js
@@ -1,4 +1,4 @@
-export const DIRECTION_LEFT = 0
-export const DIRECTION_RIGHT = 1
-export const DIRECTION_UP = 2
-export const DIRECTION_DOWN = 3
+export const DIRECTION_LEFT = 1
+export const DIRECTION_RIGHT = 2
+export const DIRECTION_UP = 3
+export const DIRECTION_DOWN = 4
